3fd985011e1320e959b4d348e0ced0d2adf0a45d
Дмитрий Сираков <dmitrysirakov@gmail.com>
2023-07-18 11:31:57 +0300

    added reaction on reply message, refactored code

--- src/send_messages.py
+++ src/send_messages.py
@@ -24,74 +24,57 @@ async def handle_msg_with_args(message, bot, classifier, ADMIN_IDS, GROUP_CHAT_I
     None
     """
 
-    if True:  # await check_user_id(message):
-        logger.info(f"Message got from new user. Checking for spam")
+    # await check_user_id(message):
+    logger.info(f"Message got from new user. Checking for spam")
 
-        try:
-            reply_to_message_id = message.reply_to_message
-        except:
-            reply_to_message_id = None
-        
-        try:
-            photo = message.photo
-        except:
-            photo = None
+    reply_to_message_id = message.reply_to_message.message_id if message.reply_to_message else None
+    photo = message.photo[-1].file_id if message.photo else None
+    text = message.text or message.caption or ""
 
-        if message.text or message.caption:
-            text = message.text or message.caption
-        else:
-            text = ""
+    print(photo)
+    print(reply_to_message_id)
+    print([text])
+    print(message.from_id)
 
-        print(photo)
-        print(reply_to_message_id)
-        print([text])
-        print(message.from_id)
-        X = {
-            "text": text,
-            "photo": photo,
-            "from_id": message.from_id,
-            "reply_to_message_id": reply_to_message_id,
-        }
-        print(X)
-        scores = classifier.predict(X)  # Wrap the message in a list
-        score = scores[0]  # Extract the score from the list
-        logger.info(f"Score: {score}")
+    X = {
+        "text": text,
+        "photo": photo,
+        "from_id": message.from_id,
+        "reply_to_message_id": reply_to_message_id,
+    }
+    print(X)
+    scores = classifier.predict(X)  # Wrap the message in a list
+    score = scores[0]  # Extract the score from the list
+    logger.info(f"Score: {score}")
 
-        treshold = 0.3
-        if score >= treshold:
-            label = "<b>&#8252;&#65039; Spam DETECTED</b>"
-            reason = f"score &#62;= {treshold}"
-        else:
-            label = "<i>No spam detected</i>"
-            reason = f"score &#60; {treshold}"
+    treshold = 0.3
+    if score >= treshold:
+        label = "<b>&#8252;&#65039; Spam DETECTED</b>"
+        reason = f"score &#62;= {treshold}"
+    else:
+        label = "<i>No spam detected</i>"
+        reason = f"score &#60; {treshold}"
 
-        logger.info(f"The message was sent to the administrator and the group")
+    logger.info("The message was sent to the administrator and the group")
  
-        spam_message_for_admins = (
-            f"{(label)} <b>({round(score * 100, 2)}%)</b>\n"
-            + "\n"
-            + f"Канал: {(message.chat.title)}\n"
-            + f"Автор: @{(message.from_user.username)}\n"
-            + f"Время: {(message.date)}\n"
-            + "\n"
-            + f"{escape(text)}\n"
-            + "\n"
-            + f"Причина: {(reason)}\n"
-        )
-
-        spam_message_for_group = spam_message_for_admins
+    spam_message_for_admins = (
+        f"{(label)} <b>({round(score * 100, 2)}%)</b>\n"
+        + "\n"
+        + f"Канал: {(message.chat.title)}\n"
+        + f"Автор: @{(message.from_user.username)}\n"
+        + f"Время: {(message.date)}\n"
+        + "\n"
+        + f"{escape(text)}\n"
+        + "\n"
+        + f"Причина: {(reason)}\n"
+    )
+    spam_message_for_group = spam_message_for_admins
+        # Send the same message to the groupы
+    if photo is None:
+        await bot.send_message(GROUP_CHAT_ID, spam_message_for_group, parse_mode="HTML")
         for admin_id in ADMIN_IDS:
-            await bot.send_message(admin_id,
-                                spam_message_for_admins,
-                                parse_mode="HTML")
-        # Send the same message to the group
-        if photo is None:
-            await bot.send_message(GROUP_CHAT_ID,
-                                spam_message_for_group,
-                                arse_mode="HTML")
-        else:
-            photo_id = photo[-1].file_id
-            await bot.send_photo(GROUP_CHAT_ID,
-                                photo=photo_id,
-                                caption=spam_message_for_group,
-                                parse_mode="HTML")
+            await bot.send_message(admin_id, spam_message_for_admins, parse_mode="HTML")
+    else:
+        await bot.send_photo(GROUP_CHAT_ID, photo=photo, caption=spam_message_for_admins, parse_mode="HTML")
+        for admin_id in ADMIN_IDS:
+            await bot.send_photo(admin_id, photo=photo, caption=spam_message_for_admins, parse_mode="HTML")
