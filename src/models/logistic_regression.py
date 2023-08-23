d4e0d20d0b087aa576f2ba2be0b5c7acdff56d5c
Дмитрий Сираков <dmitrysirakov@gmail.com>
2023-08-23 12:54:10 +0300

    added preproc data, added feature extract files for fit

--- src/models/logistic_regression.py
+++ src/models/logistic_regression.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
 from dataclasses import dataclass
 from loguru import logger
@@ -7,14 +8,20 @@ from loguru import logger
 logger.info("Init LogisticRegression")
 class SpamLogisticRegression:
     def __init__(self):
-        class_weights = {0 : 1, 1 : 837}
         self.model = LogisticRegression(penalty='l1',
-                                        class_weight=class_weights,
-                                          solver='liblinear', n_jobs=-1)
+                                        class_weight='balanced',
+                                        solver='liblinear', n_jobs=-1)
     
+    def preproc_data(self, X):
+        scale = StandardScaler()
+        normalize_data = scale.fit_transform(X)
+        return normalize_data
+
     def fit(self, X, y):
+        X = self.preproc_data(X)
         self.model.fit(X, y)
     
     def predict(self, X):
+        X = self.preproc_data(X)
         predictions = self.model.predict_proba(X)
         return predictions[:, 1]
